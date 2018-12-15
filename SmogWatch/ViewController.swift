38583b3ddc9b8e7e5476e916c9c36372458d5425
Kuba Suder <jakub.suder@gmail.com>
2018-12-15 17:26:38 +0200

    initial commit - created iOS + watchOS app

---
+++ SmogWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmogWatch
+//
+//  Created by Kuba Suder on 15.12.2018.
+//  Copyright © 2018 Kuba Suder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
