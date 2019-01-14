088f4c98f2496294136bcd20673d6c5b792c061c
Kuba Suder <jakub.suder@gmail.com>
2019-01-14 14:58:54 +0200

    removed everything that’s not essential

--- LogServer/Package.swift
+++ LogServer/Package.swift
@@ -6,14 +6,9 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
-
--- LogServer/Sources/App/routes.swift
+++ LogServer/Sources/App/routes.swift
@@ -1,26 +1,9 @@
 import Vapor
 
-/// Register your application's routes here.
 public func routes(_ router: Router) throws {
     struct LogData: Content {
         let data: String
     }
-
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 
     router.post(LogData.self, at: "logs") { (req, content) -> HTTPStatus in
         print("Received logs:")
