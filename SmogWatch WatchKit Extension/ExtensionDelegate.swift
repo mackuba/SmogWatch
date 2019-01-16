355b2b4f40862730f845c7fa595dd94d5f2d0150
Kuba Suder <jakub.suder@gmail.com>
2019-01-16 14:31:06 +0200

    added some more logging

--- SmogWatch WatchKit Extension/ExtensionDelegate.swift
+++ SmogWatch WatchKit Extension/ExtensionDelegate.swift
@@ -38,6 +38,10 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         }
     }
 
+    func applicationWillEnterForeground() {
+        NSLog("ExtensionDelegate: applicationWillEnterForeground()")
+    }
+
     func applicationDidBecomeActive() {
         NSLog("ExtensionDelegate: applicationDidBecomeActive()")
 
@@ -49,6 +53,10 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
+    }
+
+    func applicationDidEnterBackground() {
+        NSLog("ExtensionDelegate: applicationDidEnterBackground()")
     }
 
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
