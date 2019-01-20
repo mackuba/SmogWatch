57dd6caa3dfe84d03d4abdcf30b31fba09fb10f0
Kuba Suder <jakub.suder@gmail.com>
2019-01-20 22:55:37 +0200

    nest response submodels inside one another

--- SmogWatch WatchKit Extension/KrakowPiosDataLoader.swift
+++ SmogWatch WatchKit Extension/KrakowPiosDataLoader.swift
@@ -10,46 +10,46 @@ import Foundation
 
 private let DataURL = "http://monitoring.krakow.pios.gov.pl/dane-pomiarowe/pobierz"
 
-class KrakowPiosDataLoader {
-    struct DataPoint: Decodable {
-        let date: Date
-        let value: Double
-
-        struct InvalidValueError: Error {}
-
-        init(from decoder: Decoder) throws {
-            var container = try decoder.unkeyedContainer()
-
-            if let timestamp = try Int(container.decode(String.self)) {
-                self.date = Date(timeIntervalSince1970: TimeInterval(timestamp))
-            } else {
-                throw InvalidValueError()
-            }
-
-            if let value = try Double(container.decode(String.self)) {
-                self.value = value
-            } else {
-                throw InvalidValueError()
-            }
-        }
-    }
-
-    struct DataSeries: Decodable {
-        enum CodingKeys: String, CodingKey {
-            case points = "data"
-        }
-
-        let points: [DataPoint]
-    }
+private struct Response: Decodable {
+    let data: ResponseData
 
     struct ResponseData: Decodable {
         let series: [DataSeries]
-    }
 
-    struct Response: Decodable {
-        let data: ResponseData
-    }
+        struct DataSeries: Decodable {
+            enum CodingKeys: String, CodingKey {
+                case points = "data"
+            }
 
+            let points: [DataPoint]
+
+            struct DataPoint: Decodable {
+                let date: Date
+                let value: Double
+
+                struct InvalidValueError: Error {}
+
+                init(from decoder: Decoder) throws {
+                    var container = try decoder.unkeyedContainer()
+
+                    if let timestamp = try Int(container.decode(String.self)) {
+                        self.date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+                    } else {
+                        throw InvalidValueError()
+                    }
+
+                    if let value = try Double(container.decode(String.self)) {
+                        self.value = value
+                    } else {
+                        throw InvalidValueError()
+                    }
+                }
+            }
+        }
+    }
+}
+
+class KrakowPiosDataLoader {
     let dateFormatter: DateFormatter = {
         let d = DateFormatter()
         d.locale = Locale(identifier: "en_US_POSIX")
