dc9c025242174508b3189895a2ab86127bd692a7
Kuba Suder <jakub.suder@gmail.com>
2019-02-21 16:37:10 +0200

    removed logs from InterfaceController

--- SmogWatch WatchKit Extension/InterfaceController.swift
+++ SmogWatch WatchKit Extension/InterfaceController.swift
@@ -14,35 +14,18 @@ class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
-        NSLog("InterfaceController: awake(withContext:)")
-
+        
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-
-        NSLog("InterfaceController: willActivate()")
     }
-
-    override func didAppear() {
-        super.didAppear()
-
-        NSLog("InterfaceController: didAppear()")
-    }
-
-    override func willDisappear() {
-        super.willDisappear()
-
-        NSLog("InterfaceController: willDisappear()")
-    }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
-
-        NSLog("InterfaceController: didDeactivate()")
     }
+
 }
